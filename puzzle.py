ddbd2661847ee3f456cb9a36ca2eb2b9b86ab942
Andrew Lvovsky <alvov001@ucr.edu>
2018-11-04 18:36:49 -0800

    implemented uniform cost search algorithm

--- puzzle.py
+++ puzzle.py
@@ -1,4 +1,8 @@
 from random import *
+from sys import maxsize
+from copy import deepcopy
+
+num_of_children_expanded = max_depth = 0
 
 # ============ Puzzle Class ============ #
 
@@ -15,11 +19,17 @@ class Puzzle:
         if 0 not in self.puzzle_matrix[0]:
             self.swap_tiles(self.empty_pos_x, self.empty_pos_y - 1)
             self.empty_pos_y -= 1
+            return True
+        else:
+            return False
 
     def move_down(self):
         if 0 not in self.puzzle_matrix[self.size - 1]:
             self.swap_tiles(self.empty_pos_x, self.empty_pos_y + 1)
             self.empty_pos_y += 1
+            return True
+        else:
+            return False
 
     def move_left(self):
         zero_not_in_leftmost_column = True
@@ -30,6 +40,9 @@ class Puzzle:
         if zero_not_in_leftmost_column:
             self.swap_tiles(self.empty_pos_x - 1, self.empty_pos_y)
             self.empty_pos_x -= 1
+            return True
+        else:
+            return False
 
     def move_right(self):
         zero_not_in_leftmost_column = True
@@ -39,6 +52,9 @@ class Puzzle:
         if zero_not_in_leftmost_column:
             self.swap_tiles(self.empty_pos_x + 1, self.empty_pos_y)
             self.empty_pos_x += 1
+            return True
+        else:
+            return False
 
     def swap_tiles(self, x_change, y_change):
         temp = self.puzzle_matrix[self.empty_pos_y][self.empty_pos_x]
@@ -52,13 +68,14 @@ class Puzzle:
 
 
 class Node:
-    def __init__(self, puzzle):
+    def __init__(self, puzzle, path_cost):
         self.puzzle = puzzle
         self.child = []
+        self.path_cost = path_cost
 
-    def create_children(self, num_of_children, new_puzzle_state):
+    def create_children(self, num_of_children, new_puzzle_state, new_path_cost):
         for i in range(0, num_of_children):
-            self.child.append(Node(new_puzzle_state))
+            self.child.append(Node(new_puzzle_state, new_path_cost))
 
     # def set_children_values(self, list):
     #     for i in range(0, len(list)):
@@ -92,13 +109,13 @@ def init_random_puzzle(puzzle_matrix, size_of_matrix):
 
 def print_matrix(matrix):
     for row in matrix:
-        print(' '.join([str(elem) for elem in row]))
-    print("")
+        print("\t" + ' '.join([str(elem) for elem in row]))
 
 
 def run_interface():
     print("Welcome to Andrew Lvovsky's Eight-Puzzle Solver!")
     response = ""
+    puzzle = None
 
     while response != "1" and response != "2":
         print("Type '1' to use a default puzzle, or '2' to enter your own puzzle")
@@ -115,7 +132,7 @@ def run_interface():
                 [[7, 0, 2], [4, 6, 3], [8, 5, 1]],
             ]
             # Constructing Puzzle object directly
-            puzzle_matrix = Puzzle(default_puzzles[randint(0, len(default_puzzles))], 3)
+            puzzle = Puzzle(default_puzzles[randint(0, len(default_puzzles))], 3)
         elif response == "2":
             print("Enter your puzzle, use a zero to represent the blank.")
             first_row = input("Enter the first row, using a space between numbers: ")
@@ -125,7 +142,7 @@ def run_interface():
             third_row = input("Enter the third row, using a space between numbers: ")
             third_row = [int(s) for s in third_row.split() if s.isdigit()]
 
-            puzzle_matrix = Puzzle([first_row, second_row, third_row], 3)
+            puzzle = Puzzle([first_row, second_row, third_row], 3)
         else:
             print("'" + response + "' is not a valid response.")
 
@@ -137,12 +154,13 @@ def run_interface():
         print("\t2. A* w/ Misplaced Tile Heuristic")
         print("\t3. A* w/ Manhattan Distance Heuristic\n")
         response = input()
+        print("")
 
         if response == "1":
             print("Running Uniform Cost Search on")
-            puzzle_matrix.print()
-            uniform_cost_search(puzzle_matrix)
-            # run UCS
+            puzzle.print()
+            print("")
+            node = general_search(response, puzzle, queueing_function)
         elif response == "2":
             print("Running A* w/ Misplaced Tile Heuristic")
             # run A* w/ Misplaced Tile Heuristic
@@ -151,6 +169,15 @@ def run_interface():
             # run A* w/ Manhattan Distance Heuristic
         else:
             print("'" + response + "' is not a valid response.")
+
+        if node:
+            print("Winner Winner Chicken Dinner!")
+            print("")
+            print("Total nodes expanded: " + str(num_of_children_expanded))
+            print("Max num of nodes in queue at any time: ")
+            print("Depth of goal node: ")
+        else:
+            print("No solution was found.")
 
 
 def puzzle_movement_test():
@@ -172,12 +199,79 @@ def puzzle_movement_test():
             p.move_right()
         p.print()
 
+# ============ Search Functions ============ #
 
-def uniform_cost_search(puzzle_matrix):
-    node = Node(puzzle_matrix)
+
+def remove_node(list_of_nodes):
+    lowest_path_cost = index_of_node_to_remove = maxsize  # using sys.maxsize
+    for i in range(len(list_of_nodes)):
+        if list_of_nodes[i].path_cost < lowest_path_cost:
+            lowest_path_cost = list_of_nodes[i].path_cost
+            index_of_node_to_remove = i
+    node_to_return = list_of_nodes[index_of_node_to_remove]
+    list_of_nodes.pop(index_of_node_to_remove)
+    return node_to_return, index_of_node_to_remove
+
+
+def copy_new_node_into_list(node, children_list):
+    new_child = deepcopy(node)  # creates a child node by copying parent node with new legal move
+    new_child.path_cost += 1    # increments path cost (g(n))
+    children_list.append(new_child)
+
+
+def expand_node(node):
+    children = []
+
+    if node.puzzle.move_up():
+        copy_new_node_into_list(node, children)
+        node.puzzle.move_down()     # resets move to original pos so future moves can be made
+    if node.puzzle.move_down():
+        copy_new_node_into_list(node, children)
+        node.puzzle.move_up()
+    if node.puzzle.move_left():
+        copy_new_node_into_list(node, children)
+        node.puzzle.move_right()
+    if node.puzzle.move_right():
+        copy_new_node_into_list(node, children)
+        node.puzzle.move_left()
+
+    global num_of_children_expanded
+    num_of_children_expanded += len(children)
+    return children
+
+
+def queueing_function(response, index, node_list, node):
+
+    children_nodes = expand_node(node)
+
+    if response == "1":
+        for child in children_nodes:
+            node_list.insert(index, child)
+            index += 1
+        return node_list
+
+
+def general_search(response, problem, queueing_func):
+    nodes = [Node(problem, 0)]    # path_cost (g(n)) set to 0
+    while True:
+        if not nodes:   # if no nodes, return failure
+            return -1
+        (node, index) = remove_node(nodes)
+        if problem.goal_state == node.puzzle.puzzle_matrix:
+            return node
+
+        print("The best state to expand with a g(n) = " + str(node.path_cost) + " and h(n) = 0 is...")
+        node.puzzle.print()
+        print("Expanding this node...")
+        print("")
+
+        nodes = queueing_func(response, index, nodes, node)
 
 # ============ Main ============ #
 
 
-run_interface()
-# puzzle_movement_test()
+def main():
+    run_interface()
+
+
+main()
